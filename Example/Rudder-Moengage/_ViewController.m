20aa6ec2c295df671ac2603bd97dbad326ea2dc0
Abhishek Pandey <abhishekpandey10196@gmail.com>
2022-12-28 01:08:50 +0530

    feat!: update MoEngage SDK to the latest version 9.1.0

--- Example/Rudder-Moengage/_ViewController.m
+++ Example/Rudder-Moengage/_ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "_ViewController.h"
+#import <Rudder/Rudder.h>
 
 @interface _ViewController ()
 
@@ -18,6 +19,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)flush:(id)sender {
+  [[RSClient sharedInstance] flush];
+  
 }
 
 - (void)didReceiveMemoryWarning
